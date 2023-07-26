2b71da5c4a8387f3fa027458f47f74c49ca78256
Duc Dang <anhduc031100@gmail.com>
2023-07-26 07:18:42 +0100

    Finished training

--- util.py
+++ util.py
@@ -5,13 +5,9 @@ import ta
 def get_ta(df):
     df['sma'] = ta.trend.sma_indicator(df['close'], window=9)
     df['ema'] = ta.trend.ema_indicator(df['close'], window=9)
-    df['cci'] = ta.trend.cci(df['high'], df['low'], df['close'])
     # df['adx'] = ta.trend.adx(df['high'], df['low'], df['close']).replace(0,np.nan)
-    df['rsi'] = ta.momentum.rsi(df['close'])
-    df['macd'] = ta.trend.macd_diff(df['close'])
     df['obv'] = ta.volume.on_balance_volume(df['close'], df['volume'])
-    df['atr'] = ta.volatility.average_true_range(df['high'], df['low'], df['close'])
-    df.dropna(inplace=True)
+    df.fillna('bfill')
 
     return df
 
@@ -26,7 +22,7 @@ def preprocessing(df):
     ta_df.index = ta_df.date.factorize()[0]
 
     cov_list = []
-    lookback = 252*5
+    lookback = 252
 
     for i in range(lookback, len(ta_df.index.unique())):
         data_lookback = ta_df.loc[i-lookback:i,:]
